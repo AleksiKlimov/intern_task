17feec9672613867be7920429b927002f125cdd4
AleksiKlimov <leha.kurdyukov@internet.ru>
2023-02-21 11:12:29 +0300

    add new functional

--- src/components/Task1.jsx
+++ src/components/Task1.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 const Task1 = () => {
   const [state, setState] = useState(false);
-
+  console.log('event');
   const toggler = () => {
     setState(!state);
   };
